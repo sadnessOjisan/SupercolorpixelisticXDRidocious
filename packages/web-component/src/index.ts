015c96aa1870605a6ae980c7986c74e6162ee939
sadness_ojisan <sadness.ojisan@gmail.com>
2021-09-17 01:40:33 +0900

    feat: add webcomponent

--- packages/web-component/src/index.ts
+++ packages/web-component/src/index.ts
@@ -1,1 +1,22 @@
-export const a = 1;
+class Supercolorpixelisticxdridocious extends HTMLElement {
+  shadow: ShadowRoot;
+  constructor() {
+    super();
+    this.shadow = this.attachShadow({ mode: "open" });
+    this.shadow.innerHTML = `
+      <style>
+      .wrapper{
+       color: red;
+      }
+      </style>
+      <div class="wrapper">
+        aaa
+      </div>
+    `;
+  }
+}
+
+customElements.define(
+  "supercolorpixelisticxdridocious",
+  Supercolorpixelisticxdridocious
+);
