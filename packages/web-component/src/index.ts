77cb1b23bbad53d88ef8f563e3a90b54c573c2fb
sadness_ojisan <sadness.ojisan@gmail.com>
2021-09-17 03:00:48 +0900

    fix: debug font

--- packages/web-component/src/index.ts
+++ packages/web-component/src/index.ts
@@ -8,7 +8,6 @@ export class Supercolorpixelisticxdridocious extends HTMLElement {
       <style>
       span{
         display: inline;
-        font-size: 5em;
         background:linear-gradient(
             90deg,#2e4f8b 0%,#3e589d 35%,#8f619b 60%,#d0718f 70%);
         -webkit-background-clip: text;
